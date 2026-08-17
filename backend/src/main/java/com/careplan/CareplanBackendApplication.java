bd9034b012bcb90edb7a62c8739f9ffb1c2f7690
pedrqca <phcosta@gec.inatel.br>
2026-08-17 12:39:09 -0300

    chore: initialize backend with Spring Boot

---
+++ backend/src/main/java/com/careplan/CareplanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.careplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareplanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareplanBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/careplan/CareplanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareplanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
